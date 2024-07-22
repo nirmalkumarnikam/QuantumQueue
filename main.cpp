4d850365dd581890dab10843d489c16ab6551bb1
Nirmalkumar <105733313+nirmalkumarnikam@users.noreply.github.com>
2024-07-22 16:39:54 +0000

    learning to read files

--- main.cpp
+++ main.cpp
@@ -1,19 +1,44 @@
-#include<iostream>
-#include "parser.h"
-#include <bits/stdc++.h>
-#include "parser.h"
+// #include<iostream>
+// #include "parser.h"
+// #include <bits/stdc++.h>
 
-#define all(v) v.begin(), v.end()
 
+// #define all(v) v.begin(), v.end()
+
+// using namespace std;
+
+// //ya goshtin var code kaam karel...  hope so will make all these algos work
+// const string TRACE = "trace";
+// const string SHOW_STATISTICS = "stats";
+// const string ALGORITHMS[9] = {"", "FCFS", "RR-", "SPN", "SRT", "HRRN", "FB-1", "FB-2i", "AGING"};
+
+
+// int main(){
+//     cout<< "CPU Scheduling Simulator"<<endl;
+//     return 0;
+// }
+
+
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "parser.h"
 using namespace std;
 
-//ya goshtin var code kaam karel...  hope so will make all these algos work
-const string TRACE = "trace";
-const string SHOW_STATISTICS = "stats";
-const string ALGORITHMS[9] = {"", "FCFS", "RR-", "SPN", "SRT", "HRRN", "FB-1", "FB-2i", "AGING"};
 
+int main() {
+    ifstream inputFile("input.txt");
+    if (!inputFile) {
+        cerr << "Error opening file" << endl;
+        return 1;
+    }
 
-int main(){
-    cout<< "CPU Scheduling Simulator"<<endl;
+    string line;
+    while (getline(inputFile, line)) {
+        cout<<"slkadslf;la";
+    }
+
+    inputFile.close();
     return 0;
 }
--- parser.h
+++ parser.h
@@ -1,15 +1,15 @@
-#ifndef PARSER_H_INCLUDED
-#define PARSER_H_INCLUDED
-#include <bits/stdc++.h>
-using namespace std;
-string operation;
-int last_instant, process_count;
-vector<pair<char, int>> algorithms;
-vector<tuple<string,int,int>> processes;
-vector<vector<char>>timeline;
-unordered_map<string,int>processToIndex;
+// #ifndef PARSER_H_INCLUDED
+// #define PARSER_H_INCLUDED
+// #include <bits/stdc++.h>
+// using namespace std;
+// string operation;
+// int last_instant, process_count;
+// vector<pair<char, int>> algorithms;
+// vector<tuple<string,int,int>> processes;
+// vector<vector<char>>timeline;
+// unordered_map<string,int>processToIndex;
 
-//Results
-vector<int>finishTime;
-vector<int>turnAroundTime;
-vector<float>normTurn;
+// //Results
+// vector<int>finishTime;
+// vector<int>turnAroundTime;
+// vector<float>normTurn;
