133c2f0ef0e0732957376c908fada432c52dc837
Nirmalkumar <105733313+nirmalkumarnikam@users.noreply.github.com>
2024-07-22 15:53:14 +0000

    initialized some global variable in parse and defined bit of structure in main.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
 #include<iostream>
 #include "parser.h"
+#include <bits/stdc++.h>
+#include "parser.h"
+
+#define all(v) v.begin(), v.end()
 
 using namespace std;
+
+//ya goshtin var code kaam karel...  hope so will make all these algos work
+const string TRACE = "trace";
+const string SHOW_STATISTICS = "stats";
+const string ALGORITHMS[9] = {"", "FCFS", "RR-", "SPN", "SRT", "HRRN", "FB-1", "FB-2i", "AGING"};
+
 
 int main(){
     cout<< "CPU Scheduling Simulator"<<endl;
