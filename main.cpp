f24380adf7995e3acc8d255a4fd6751f1da5cdb0
Nirmalkumar <105733313+nirmalkumarnikam@users.noreply.github.com>
2024-07-23 07:26:30 +0000

    still working with parser... this will be difficult

--- main.cpp
+++ main.cpp
@@ -1,44 +1,11 @@
-// #include<iostream>
-// #include "parser.h"
-// #include <bits/stdc++.h>
-
-
-// #define all(v) v.begin(), v.end()
-
-// using namespace std;
-
-// //ya goshtin var code kaam karel...  hope so will make all these algos work
-// const string TRACE = "trace";
-// const string SHOW_STATISTICS = "stats";
-// const string ALGORITHMS[9] = {"", "FCFS", "RR-", "SPN", "SRT", "HRRN", "FB-1", "FB-2i", "AGING"};
-
-
-// int main(){
-//     cout<< "CPU Scheduling Simulator"<<endl;
-//     return 0;
-// }
-
-
-
+#include <bits/stdc++.h>
 #include <iostream>
-#include <fstream>
-#include <string>
 #include "parser.h"
 using namespace std;
 
 
-int main() {
-    ifstream inputFile("input.txt");
-    if (!inputFile) {
-        cerr << "Error opening file" << endl;
-        return 1;
-    }
-
-    string line;
-    while (getline(inputFile, line)) {
-        cout<<"slkadslf;la";
-    }
-
-    inputFile.close();
+int main()
+{
+    parse();
     return 0;
 }
--- parser.h
+++ parser.h
@@ -1,21 +1,45 @@
 #ifndef PARSER_H_INCLUDED
 #define PARSER_H_INCLUDED
 
-
-#include <bits/stdc++.h>
-
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <string>
+#include <tuple>
 
 using namespace std;
 
-
-string operation;                           
+string operation;
 int last_instant, process_count;
-vector<pair<char, int>> algorithms;         //algo id & quantum time
-vector<tuple<string,int,int>> processes;    //name & arrvl time & srvicce time
-vector<vector<char>>timeline;
-unordered_map<string,int>processToIndex;    //ye switch cases mei kaam ayega
+vector<pair<char, int>> algorithms; // algo id & quantum time
+vector<tuple<string, int, int>> processes; // name, arrival time, service time
+vector<vector<char>> timeline;
+unordered_map<string, int> processToIndex; // this will be used in switch cases
 
-//Results
-vector<int>finishTime;
-vector<int>turnAroundTime;
-vector<float>normTurn;
+// Results
+vector<int> finishTime;
+vector<int> turnAroundTime;
+vector<float> normTurn;
+
+void parse()
+{
+    string algorithm_chunk;
+    cin >> operation >> algorithm_chunk >> last_instant >> process_count;
+    
+    // Debug statements to check input
+    cout << "Operation: " << operation << endl;
+    cout << "Algorithm Chunk: " << algorithm_chunk << endl;
+    cout << "Last Instant: " << last_instant << endl;
+    cout << "Process Count: " << process_count << endl;
+    
+
+    if (cin.fail()) {
+        cerr << "Error reading input." << endl;
+        return;
+    }
+
+    // Debug statements to check initialization
+    cout << "Initialization complete." << endl;
+}
+
+#endif // PARSER_H_INCLUDED
