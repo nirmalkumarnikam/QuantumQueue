255037a7b3af69e9b5119b642a3d21eb942f67de
Nirmalkumar <105733313+nirmalkumarnikam@users.noreply.github.com>
2024-07-23 06:10:54 +0000

    Brainstromed the work flow and had the code flow (flowchart done) learned about parser and file input techniques.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # QuantumQueue
 Here I am Making CPU Scheduling Algorithm
+
+
+Code Flow:
+
+Day 0: 
+tried and tested all the software and extentions
+
+Day 1:
+Brainstromed the work flow and had the code flow (flowchart done)
+learned about parser and file input techniques.
--- parser.h
+++ parser.h
@@ -1,15 +1,21 @@
-// #ifndef PARSER_H_INCLUDED
-// #define PARSER_H_INCLUDED
-// #include <bits/stdc++.h>
-// using namespace std;
-// string operation;
-// int last_instant, process_count;
-// vector<pair<char, int>> algorithms;
-// vector<tuple<string,int,int>> processes;
-// vector<vector<char>>timeline;
-// unordered_map<string,int>processToIndex;
+#ifndef PARSER_H_INCLUDED
+#define PARSER_H_INCLUDED
 
-// //Results
-// vector<int>finishTime;
-// vector<int>turnAroundTime;
-// vector<float>normTurn;
+
+#include <bits/stdc++.h>
+
+
+using namespace std;
+
+
+string operation;                           
+int last_instant, process_count;
+vector<pair<char, int>> algorithms;         //algo id & quantum time
+vector<tuple<string,int,int>> processes;    //name & arrvl time & srvicce time
+vector<vector<char>>timeline;
+unordered_map<string,int>processToIndex;    //ye switch cases mei kaam ayega
+
+//Results
+vector<int>finishTime;
+vector<int>turnAroundTime;
+vector<float>normTurn;
