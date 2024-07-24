51eb3239f03449d0004135fe923ca5cbd9503b07
Nirmalkumar <105733313+nirmalkumarnikam@users.noreply.github.com>
2024-07-24 16:30:27 +0000

    FCFS implementation  done

--- main.cpp
+++ main.cpp
@@ -1,12 +1,51 @@
 #include <bits/stdc++.h>
-#include <iostream>
 #include "parser.h"
 using namespace std;
 
-void firstComeFirstServe(){
-    
+
+void clear_timeline()
+{
+    for(int i=0; i<last_instant; i++)
+        for(int j=0; j<process_count; j++)
+            timeline[i][j] = ' ';
 }
-void roundRobin(){
+
+
+string getProcessName(tuple<string, int, int> &a)
+{
+    return get<0>(a);
+}
+int getArrivalTime(tuple<string, int, int> &a)
+{
+    return get<1>(a);
+}
+int getServiceTime(tuple<string, int, int> &a)
+{
+    return get<2>(a);
+}
+
+
+void firstComeFirstServe()
+{
+    int time = getArrivalTime(processes[0]);
+    for (int i = 0; i < process_count; i++)
+    {
+        int processIndex = i;
+        int arrivalTime = getArrivalTime(processes[i]);
+        int serviceTime = getServiceTime(processes[i]);
+
+        finishTime[processIndex] = (time + serviceTime);
+        turnAroundTime[processIndex] = (finishTime[processIndex] - arrivalTime);
+        normTurn[processIndex] = (turnAroundTime[processIndex] * 1.0 / serviceTime);
+
+        for (int j = time; j < finishTime[processIndex]; j++)
+            timeline[j][processIndex] = '*';
+        for (int j = arrivalTime; j < time; j++)
+            timeline[j][processIndex] = '.';
+        time += serviceTime;
+    }
+}
+void roundRobin(int originalQuantum){
 
 }
 void shortestProcessNext(){
@@ -24,7 +63,7 @@ void feedbackQ1(){
 void feedbackQ2i(){
 
 }
-void aging(){
+void aging(int originalQuantum){
 
 }
 
@@ -34,10 +73,11 @@ void execute_algorithm(char algorithm_id, int quantum,string operation)
     switch (algorithm_id)
     {
     case '1':
+        cout<<"FCFS ";
         firstComeFirstServe();
         break;
     case '2':
-        roundRobin();
+        roundRobin(quantum);
         break;
     case '3':
         shortestProcessNext();
@@ -55,7 +95,7 @@ void execute_algorithm(char algorithm_id, int quantum,string operation)
         feedbackQ2i();
         break;
     case '8':
-        aging();
+        aging(quantum);
         break;
     default:
         break;
@@ -63,8 +103,39 @@ void execute_algorithm(char algorithm_id, int quantum,string operation)
 }
 
 
+void printTimeline(int algorithm_index)
+{
+    for (int i = 0; i <= last_instant; i++)
+        cout << i % 10<<" ";
+    cout <<"\n";
+    cout << "------------------------------------------------\n";
+    for (int i = 0; i < process_count; i++)
+    {
+        cout << getProcessName(processes[i]) << "     |";
+        for (int j = 0; j < last_instant; j++)
+        {
+            cout << timeline[j][i]<<"|";
+        }
+        cout << " \n";
+    }
+    cout << "------------------------------------------------\n";
+}
+
+//future scope ke liye 
+void printStats(){
+
+}
+
+
 int main()
 {
     parse();
+    for (int i = 0; i < (int)algorithms.size(); i++)
+    {
+        clear_timeline();
+        execute_algorithm(algorithms[i].first, algorithms[i].second,operation);
+        printTimeline(i);
+        cout << "\n";
+    }   
     return 0;
 }
